b5b8feafdf7e83e247e87d461565be288104d802
Kalbin Vladimir <kalbin.wow@yandex.ru>
2022-03-01 23:20:08 +0500

    Updated adminLayout.

--- src/main/java/org/kalbinvv/tsclient/layout/AdminLayout.java
+++ src/main/java/org/kalbinvv/tsclient/layout/AdminLayout.java
@@ -2,6 +2,7 @@ package org.kalbinvv.tsclient.layout;
 
 import java.io.IOException;
 
+
 import org.kalbinvv.tsclient.Config;
 import org.kalbinvv.tsclient.TsClient;
 import org.kalbinvv.tscore.net.Connection;
@@ -34,6 +35,7 @@ public class AdminLayout extends Layout{
 		TextField passField = new TextField();
 		passField.setPromptText("Пароль");
 		Button addUserButton = new Button("Добавить пользователя!");
+		Button addAdminUserButton = new Button("Добавить пользователя с административными правами!");
 		addUserButton.setOnAction((ActionEvent event) -> {
 			UserEntry userEntry = new UserEntry(loginField.getText(), passField.getText());
 			try {
@@ -60,10 +62,38 @@ public class AdminLayout extends Layout{
 				alert.showAndWait();
 			}
 		});
+		addAdminUserButton.setOnAction((ActionEvent event) -> {
+			UserEntry userEntry = new UserEntry(loginField.getText(), passField.getText());
+			try {
+				Config config = TsClient.getConfig();
+				Connection connection = new Connection(config.getServerAddress()
+						.toSocket());
+				Response response = connection.sendRequestAndGetResponse(
+						new Request(RequestType.AddAdminUser, userEntry, 
+						config.getUser()));
+				if(response.getType() == ResponseType.Successful) {
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setContentText("Пользователь успешно добавлен!");
+					alert.showAndWait();
+				}else {
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setContentText("Не удалось добавить пользователя!\n"
+							+ (String)response.getObject());
+					alert.showAndWait();
+				}
+			} catch (IOException e) {
+				String errorMsg = e.getMessage();
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setContentText("Не удалось добавить пользователя!\n" + errorMsg);
+				alert.showAndWait();
+			}
+		});
 		addNode(addText);
 		addNode(loginField);
 		addNode(passField);
 		addNode(addUserButton);
+		addNode(addAdminUserButton);
 	}
+	
 
 }
