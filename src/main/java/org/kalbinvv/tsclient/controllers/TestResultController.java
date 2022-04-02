887a62c3168894a13edffb913421d398ac66e247
Kalbin Vladimir <kalbin.wow@yandex.ru>
2022-04-03 02:41:15 +0500

    Edit testResult controller.

--- src/main/java/org/kalbinvv/tsclient/controllers/TestResultController.java
+++ src/main/java/org/kalbinvv/tsclient/controllers/TestResultController.java
@@ -1,6 +1,7 @@
 package org.kalbinvv.tsclient.controllers;
 
 import java.net.URL;
+
 import java.util.ResourceBundle;
 
 import org.kalbinvv.tsclient.TsClient;
@@ -9,7 +10,6 @@ import org.kalbinvv.tscore.test.TestResult;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.PieChart;
 import javafx.scene.text.Text;
 
 public class TestResultController implements Initializable{
@@ -19,8 +19,6 @@ public class TestResultController implements Initializable{
 	@FXML
 	private Text resultPercent;
 
-	@FXML
-	private PieChart chart;
 
 	public TestResultController(TestResult testResultObject) {
 		this.testResultObject = testResultObject;
@@ -28,16 +26,8 @@ public class TestResultController implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		PieChart.Data correctData = new PieChart.Data("Правильные ответы", 
-				testResultObject.getAmountOfCorrectAnswers());
-		int notCorrect = testResultObject.getAmountOfAnswers() - testResultObject.getAmountOfCorrectAnswers();
-		chart.getData().add(correctData);
-		if(notCorrect > 0) {
-			PieChart.Data notCorrectData = new PieChart.Data("Неправильные ответы", 
-					testResultObject.getAmountOfAnswers() - testResultObject.getAmountOfCorrectAnswers());
-			chart.getData().add(notCorrectData);
-		}
-		resultPercent.setText("Финальный результат: " + Math.floor(Double.valueOf(testResultObject.getAmountOfCorrectAnswers()) 
+		resultPercent.setText(Math.floor(Double.valueOf(testResultObject.
+				getAmountOfCorrectAnswers()) 
 				/ testResultObject.getAmountOfAnswers() * 100) + "%");
 	}
 
